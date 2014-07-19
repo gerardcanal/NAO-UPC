929c7e02b116192dd08eadbb44ae48203d6051d1
AlbertPumarola <pumapumarola92@gmail.com>
2014-07-19 12:45:22 +0200

    add tomatoe tracker

---
+++ opencv/object_tracking.cpp
@@ -0,0 +1,83 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <stdio.h>
+#include <ctype.h>
+#include <iostream>
+#include <math.h>
+
+using namespace cv;
+using namespace std;
+
+int lowerH=0;
+int lowerS=146;
+int lowerV=40;
+int upperH=180;
+int upperS=256;
+int upperV=256;
+
+
+//This function threshold the HSV image and create a binary image
+Mat GetThresholdedImage(Mat imgHSV)
+{
+	Mat imgThresh;
+	inRange(imgHSV, Scalar(lowerH,lowerS,lowerV), Scalar(upperH,upperS,upperV), imgThresh);
+	return imgThresh;
+}
+
+//This function tracks the object with bigger area 
+void trackObject(Mat imgThresh, Point2f &center, float &area)
+{
+  //Get the moments
+  Moments mu = moments( imgThresh, true ); 
+
+  //Get the mass center:
+  Point2f mc;
+  if (mu.m00 > 2000){//if area<2000 is noise
+  	mc = Point2f( static_cast<float>(mu.m10/mu.m00) , static_cast<float>(mu.m01/mu.m00) ); 
+  }else{
+  	mc = Point2f(static_cast<float>(0),static_cast<float>(0));
+  }
+  //printf(" Area (M_00) = %.2f  \n", mu.m00);
+  //printf(" POSX = %.2f  POSY = %.2f \n", mc.x, mc.y);
+  area = mu.m00;
+  center = mc;
+}
+
+int main(){
+	CvCapture* capture = cvCaptureFromCAM(0);
+
+	if(!capture){
+		printf("Capture failure\n");
+		return -1;
+	}
+
+	namedWindow("Tomatoe", CV_WINDOW_KEEPRATIO);
+
+	//iterate through each frames of the video
+	while(true){
+
+		Mat frame = cvQueryFrame(capture);
+
+		Mat imgHSV;
+		cvtColor(frame, imgHSV, CV_BGR2HSV); //Change the color format from BGR to HSV
+
+		Mat imgThresh = GetThresholdedImage(imgHSV);
+
+		blur( imgThresh, imgThresh, Size(3,3) );
+
+		Point2f obj_pos;
+		float area;
+		trackObject(imgThresh, obj_pos, area);
+		circle(frame, obj_pos, 10, Scalar(20,237,0), -1 , 8, 0);
+		circle(frame, obj_pos, sqrt(area/M_PI), Scalar(20,237,0), 2, 8, 0);
+		imshow("Tomatoe", frame);
+
+
+		//Wait 80mS
+		int c = cvWaitKey(80);
+		//If 'ESC' is pressed, break the loop
+		if((char)c==27 ) break;
+	}
+	return 0;
+}
---
+++ opencv/select_threshold_values.cpp
@@ -0,0 +1,71 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <stdio.h>
+#include <ctype.h>
+#include <iostream>
+
+
+using namespace cv;
+using namespace std;
+
+int lowerH=0;
+int lowerS=0;
+int lowerV=0;
+int upperH=0;
+int upperS=0;
+int upperV=0;
+
+int maxH=180;
+int maxS=256;
+int maxV=256;
+
+
+//This function threshold the HSV image and create a binary image
+Mat GetThresholdedImage(Mat imgHSV){
+	Mat imgThresh;
+	inRange(imgHSV, Scalar(lowerH,lowerS,lowerV), Scalar(upperH,upperS,upperV), imgThresh);
+	return imgThresh;
+}
+
+//This function create two windows and 6 trackbars for the "Ball" window
+void setTrackbars(){
+
+	createTrackbar("LowerH", "Ball", &lowerH, maxH, NULL);
+	        createTrackbar("UpperH", "Ball", &upperH, maxH, NULL);
+
+	createTrackbar("LowerS", "Ball", &lowerS, maxS, NULL);
+	        createTrackbar("UpperS", "Ball", &upperS, maxS, NULL);
+
+	createTrackbar("LowerV", "Ball", &lowerV, maxV, NULL);
+	        createTrackbar("UpperV", "Ball", &upperV, maxV, NULL);
+}
+
+int main(){
+	CvCapture* capture = cvCaptureFromCAM(0);
+	if(!capture){
+		printf("Capture failure\n");
+		return -1;
+	}
+	namedWindow("Video");
+	namedWindow("Ball", CV_WINDOW_KEEPRATIO);
+	Mat frame;
+	setTrackbars();
+	//iterate through each frames of the video
+	while(true){
+		frame = cvQueryFrame(capture);
+		Mat imgHSV;
+		cvtColor(frame, imgHSV, CV_BGR2HSV); //Change the color format from BGR to HSV
+		Mat imgThresh = GetThresholdedImage(imgHSV);
+		imshow("Ball", imgThresh);
+		//imshow("Video", frame);
+
+		//Wait 80mS
+		int c = cvWaitKey(80);
+		//If 'ESC' is pressed, break the loop
+		if((char)c==27 ) break;
+	}
+	cvDestroyAllWindows();
+	cvReleaseCapture(&capture);
+	return 0;
+}
