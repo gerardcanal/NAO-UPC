df4bba499d73ea3207c14dd14e76ae7ad27cb5c8
AlbertPumarola <pumapumarola92@gmail.com>
2014-07-20 18:57:45 +0200

    qr_tracker add

---
+++ opencv/qr_tracker.cpp
@@ -0,0 +1,73 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <stdio.h>
+#include <zbar.h>
+#include <string>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+using namespace zbar;
+
+#define FONT		FONT_HERSHEY_PLAIN
+#define FONT_COLOR_DATA	Scalar(255,0,255)
+#define LINE_TYPE	CV_AA
+
+int main(){
+	CvCapture* capture = cvCaptureFromCAM(0);
+
+	if(!capture){
+		printf("Capture failure\n");
+		return -1;
+	}
+
+	namedWindow("QR", CV_WINDOW_KEEPRATIO);
+
+	
+    	ImageScanner scanner;
+    	scanner.set_config(ZBAR_QRCODE, ZBAR_CFG_ENABLE, 1);
+
+	//iterate through each frames of the video
+	while(true){
+
+		Mat frame = cvQueryFrame(capture);
+
+		// Convert to grayscale
+		Mat frame_grayscale;
+        	cvtColor(frame, frame_grayscale, CV_BGR2GRAY);
+
+		// wrap image
+		int width = frame_grayscale.cols;
+		int height = frame_grayscale.rows;
+		uchar *raw = (uchar *)(frame_grayscale.data);
+		Image image(width, height, "Y800", raw, width * height);
+
+		// Scan the image for QRcodes
+		scanner.scan(image);
+		for (Image::SymbolIterator symbol = image.symbol_begin(); symbol != image.symbol_end(); ++symbol) {
+		    //Size
+		    int size_x = symbol->get_location_x(2) - symbol->get_location_x(1);
+		    int size_y = symbol->get_location_y(1) - symbol->get_location_y(0);
+		    putText(frame, format("Size: %ix%i", size_x, size_y), cvPoint(10, frame.rows-70), FONT, 2, FONT_COLOR_DATA, 1.5, LINE_TYPE);
+		    //Data
+		    putText(frame, format("Data: %s", symbol->get_data().c_str()), cvPoint(10, frame.rows-30), FONT, 2, FONT_COLOR_DATA, 1.5, LINE_TYPE);
+		    // Location
+		    if (symbol->get_location_size() == 4) {
+		        line(frame, Point(symbol->get_location_x(0), symbol->get_location_y(0)), Point(symbol->get_location_x(1), symbol->get_location_y(1)), Scalar(255,0,0), 2, 8, 0);
+		        line(frame, Point(symbol->get_location_x(1), symbol->get_location_y(1)), Point(symbol->get_location_x(2), symbol->get_location_y(2)), Scalar(0,255,0), 2, 8, 0);
+		        line(frame, Point(symbol->get_location_x(2), symbol->get_location_y(2)), Point(symbol->get_location_x(3), symbol->get_location_y(3)), Scalar(0,0,255), 2, 8, 0);
+		        line(frame, Point(symbol->get_location_x(3), symbol->get_location_y(3)), Point(symbol->get_location_x(0), symbol->get_location_y(0)), Scalar(0,255,255), 2, 8, 0);
+		    }
+		}
+
+		imshow("QR", frame);
+
+
+		//Wait 80mS
+		int c = cvWaitKey(80);
+		//If 'ESC' is pressed, break the loop
+		if((char)c==27 ) break;
+	}
+	return 0;
+}
