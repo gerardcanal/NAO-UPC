45a33db512e7f9d714eea2c7b99dfc5a9dd1a874
AlbertPumarola <pumapumarola92@gmail.com>
2014-09-03 17:01:00 +0200

    for opencv2

--- utils/tomato_tracker/src/tomatoTracker.cpp
+++ utils/tomato_tracker/src/tomatoTracker.cpp
@@ -23,8 +23,7 @@ TomatoTracker::TomatoTracker(int lowerH, int lowerS, int lowerV, int upperH, int
 void TomatoTracker::track(Mat frame, Point2f &obj_pos, float &area)
 {
 	Mat imgHSV;
-	//cvtColor(frame, imgHSV, CV_BGR2HSV); 
-	cvtColor(frame, imgHSV, COLOR_BGR2HSV);
+	scvtColor(frame, imgHSV, CV_BGR2HSV); 
 
 	Mat imgThresh = getThresholdedImage(imgHSV);
 
