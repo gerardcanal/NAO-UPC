10bb12b4060dc9b93a5fb972a5738622bf62a674
Edgar <eriba@eriba-VirtualBox.(none)>
2014-09-17 13:21:26 +0200

    Task2 with ServiceState

--- tasks/smach_shopping_list/src/smach_shopping_list.py
+++ tasks/smach_shopping_list/src/smach_shopping_list.py
@@ -45,35 +45,56 @@ class Task1(smach.State):
         else:
             return 'succeeded'        
 
-
+'''
 # define state Task2 - Shopping list
 class Task2(smach.State):
     def __init__(self):
         smach.State.__init__(self, outcomes=['succeeded','aborted','preempted'])
         self.counter = 0
-        self.max_counter = 2
-        self._ss = ServiceState('shopping_list/checkObjects', checkObjects, response_cb = self.response_cb)
+        self.max_counter = 3
 
     def execute(self, userdata):
         rospy.loginfo('Executing state TASK 2')
-        
         if random.randint(0, 4*self.max_counter) > self.max_counter:
             return 'aborted'
         elif self.counter < self.max_counter:
-            self._ss.execute(userdata)    
+            self.counter += 1
+            rospy.sleep(2.)
+            return 'preempted'   
         else:
+            return 'succeeded' 
+'''
+# define state Task2 - Shopping list
+class Task2(ServiceState):
+    def __init__(self, name, spec):
+        ServiceState.__init__(self, service_name=name,
+                                    service_spec=spec,
+                                    response_cb=self.response_cb)
+        self.counter = 0
+        self.max_counter = 3
+        self.reponse = 0
+
+
+    def execute(self, userdata):
+        rospy.loginfo('Executing state TASK 2')
+        if self.counter > self.max_counter:
             return 'aborted'
-  
-    def response_cb(self, msg):
-
-        print msg
-        print msg.res.num_objects
-        print msg.res.shopping_list
-
-        if msg.res.num_objects <= 0:
-            return 'preempted'
         else:
-            return 'succeded'
+            ServiceState.execute(self, userdata)
+            if self.response == 0:
+                return 'succeeded'
+            elif self.response == 1:
+                return 'preempted'
+            else:
+                rospy.loginfo('Something is wrong')
+
+    def response_cb(self, userdata, response):
+        print response
+        if response.num_objects > 0:
+            self.response = 0 # succeeded
+        else:
+            self.counter += 1
+            self.response = 1 # preempted
 
 
 # define state Task3 - Meal preparation
@@ -107,17 +128,25 @@ def main():
 
     # Open the container
     with sm:
+
         # Add states to the container
         smach.StateMachine.add('IDLE', Idle(), 
                                transitions={'toTask1':'TASK1', 'toTask2':'TASK2', 'toTask3':'TASK3'},
                                remapping={'idle_input':'task_init'})
+
+
         smach.StateMachine.add('TASK1', Task1(), 
                                transitions={'succeeded':'TASK2', 'aborted':'IDLE', 'preempted':'TASK1'})
-        smach.StateMachine.add('TASK2', Task2(), 
+
+        
+        smach.StateMachine.add('TASK2', Task2('shopping_list/checkObjects', checkObjects), 
                                transitions={'succeeded':'TASK3', 'aborted':'IDLE', 'preempted':'TASK2'})
+
+       
         smach.StateMachine.add('TASK3', Task3(), 
                                transitions={'succeeded':'FINISH', 'aborted':'IDLE', 'preempted':'TASK3'})
     
+
     # Create and start the introspection server
     sis = smach_ros.IntrospectionServer('server_name', sm, '/SM_ROOT')
     sis.start()
