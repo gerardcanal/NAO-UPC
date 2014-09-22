bcd640884a5a70cd1e61ea6abaaf37ad8add9842
Edgar <eriba@eriba-VirtualBox.(none)>
2014-09-22 11:22:34 +0200

    Checking with Timer OK

--- tasks/smach_shopping_list/src/smach_shopping_list.py
+++ tasks/smach_shopping_list/src/smach_shopping_list.py
@@ -4,51 +4,36 @@ import roslib; roslib.load_manifest('smach_shopping_list')
 import rospy
 import smach
 import smach_ros
-
+# Node messages
 from smach_ros import ServiceState
 from shopping_list.srv import checkObjects
+# Check node life
+import os
+import xmlrpclib
 
-import random # for simulation
-
-
-# define state IDLE - Waiting state
-# this state could be changeg by a concurrence SM
-class Idle(smach.State):
-    def __init__(self):
-        smach.State.__init__(self, outcomes=['toTask1','toTask2','toTask3'], 
-                                   input_keys=['idle_input'])
-
-    def execute(self, userdata):
-        rospy.loginfo('IDLE')
-        if userdata.idle_input == 1:  
-            return 'toTask1'
-        elif userdata.idle_input == 2:  
-            return 'toTask2'
-        elif userdata.idle_input == 3:  
-            return 'toTask3'
-
-
-# define state Task1 - Put off the fire
-class Task1(smach.State):
-    def __init__(self):
-        smach.State.__init__(self, outcomes=['succeeded','aborted','preempted'])
-        self.counter = 0
-        self.max_counter = 5
+# define state CheckService - Waiting state
+# this state check if the service server is running
+class CheckService(smach.State):
+    def __init__(self, serviceName):
+        smach.State.__init__(self, outcomes=['succeeded','aborted', 'preempted'])
+        self._serviceName = serviceName
+        self._sp = xmlrpclib.ServerProxy(os.environ['ROS_MASTER_URI'])
+        self.timeinit = rospy.Time.now()
+        self.timeout = rospy.Duration(100)
 
     def execute(self, userdata):
-        rospy.loginfo('Executing state TASK 1')
-        if random.randint(0, 4*self.max_counter) > self.max_counter:
-            return 'aborted'
-        elif self.counter < self.max_counter:
-            self.counter += 1
-            rospy.sleep(1.)
-            return 'preempted'   
+        rospy.loginfo('CheckService')
+        code, msg, val = self._sp.lookupService('shopping_list_server', self._serviceName)
+        if code == 1:
+            return 'succeeded'
+        elif code != 1 and rospy.Time.now() - self.timeinit < self.timeout:
+            return 'preempted'
         else:
-            return 'succeeded'        
+            return 'aborted'   
 
 
 # define state Task2 - Shopping list
-class Task2(ServiceState):
+class Task(ServiceState):
     def __init__(self, name, spec):
         ServiceState.__init__(self, service_name=name,
                                     service_spec=spec,
@@ -80,55 +65,25 @@ class Task2(ServiceState):
             self.response = 1 # preempted
 
 
-# define state Task3 - Meal preparation
-class Task3(smach.State):
-    def __init__(self):
-        smach.State.__init__(self, outcomes=['succeeded','aborted','preempted'])
-        self.counter = 0
-        self.max_counter = 2
-
-    def execute(self, userdata):
-        rospy.loginfo('Executing state TASK 3')
-        if random.randint(0, 4*self.max_counter) > self.max_counter:
-            return 'aborted'
-        elif self.counter < self.max_counter:
-            self.counter += 1
-            rospy.sleep(1.)
-            return 'preempted'   
-        else:
-            return 'succeeded'  
-        
-
 
 def main():
     rospy.init_node('smach_humabot')
 
     # Create a SMACH state machine
-    sm = smach.StateMachine(outcomes=['FINISH'])
-    sm.userdata.task_init = 1 
-    sm.userdata.sm_counter = 0 # till now not used
-    sm.userdata.last_state = 1 # till now not used
+    sm = smach.StateMachine(outcomes=['succeeded','aborted','preempted'])
+    sm.service = 'shopping_list/checkObjects'
 
     # Open the container
     with sm:
 
         # Add states to the container
-        smach.StateMachine.add('IDLE', Idle(), 
-                               transitions={'toTask1':'TASK1', 'toTask2':'TASK2', 'toTask3':'TASK3'},
-                               remapping={'idle_input':'task_init'})
-
-
-        smach.StateMachine.add('TASK1', Task1(), 
-                               transitions={'succeeded':'TASK2', 'aborted':'IDLE', 'preempted':'TASK1'})
+        smach.StateMachine.add('CHECK', CheckService(sm.service), 
+                               transitions={'succeeded':'TASK', 'aborted':'aborted', 'preempted':'CHECK'})
 
         
-        smach.StateMachine.add('TASK2', Task2('shopping_list/checkObjects', checkObjects), 
-                               transitions={'succeeded':'TASK3', 'aborted':'IDLE', 'preempted':'TASK2'})
-
-       
-        smach.StateMachine.add('TASK3', Task3(), 
-                               transitions={'succeeded':'FINISH', 'aborted':'IDLE', 'preempted':'TASK3'})
-    
+        smach.StateMachine.add('TASK', Task(sm.service, checkObjects), 
+                               transitions={'succeeded':'succeeded', 'aborted':'aborted', 'preempted':'TASK'})
+   
 
     # Create and start the introspection server
     sis = smach_ros.IntrospectionServer('server_name', sm, '/SM_ROOT')
