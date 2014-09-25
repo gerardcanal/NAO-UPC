81fdaebff1cca22dddbb3112282f48af102bbed3
Gerard Canal <grar.knal@gmail.com>
2014-09-25 17:09:40 +0200

    Changed emergency stop to use the nao_sensors. Also modified the launch file to launch the publisher from that node.

--- nao_smach_utils/src/emergency_stop.py
+++ nao_smach_utils/src/emergency_stop.py
@@ -33,7 +33,7 @@ def tactile_touch_callback(data, publisher):
 
 OFFSET = 0.045 # m offset from min_range
 def sonar_cb(data, arg):
-    print data.range, data.min_range+OFFSET
+    #print data.range, data.min_range+OFFSET
     if data.range <= data.min_range+OFFSET: 
         publisher = arg[0]
         publisher.publish(Twist(Vector3(0.0, 0.0, 0.0), Vector3(0.0, 0.0, 0.0)));
@@ -47,8 +47,10 @@ if __name__ == '__main__':
     rospy.Subscriber("bumper", Bumper, bumper_callback, callback_args=pub)
     rospy.Subscriber("tactile_touch", TactileTouch, tactile_touch_callback, callback_args=pub)
     if USE_SONAR:
-        rospy.Subscriber("nao_sonar/right", Range, sonar_cb, callback_args=[pub, 'Right'], queue_size=10)
-        rospy.Subscriber("nao_sonar/left", Range, sonar_cb, callback_args=[pub, 'Left'], queue_size=10)
+        left_topic = '/nao/sonar_left'   # nao_sonar/left -- mine
+        right_topic = '/nao/sonar_right' # nao_sonar/right -- mine
+        rospy.Subscriber(right_topic, Range, sonar_cb, callback_args=[pub, 'Right'], queue_size=10)
+        rospy.Subscriber(left_topic, Range, sonar_cb, callback_args=[pub, 'Left'], queue_size=10)
 
     speak = rospy.Publisher('/speech', String, latch=True)
     speak.publish(String('Emergency Stop has been enabled!'))
