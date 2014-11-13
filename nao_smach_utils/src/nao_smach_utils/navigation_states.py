0bf9df1650235f71becc4977cbc71be35fce0275
Gerard Canal <grar.knal@gmail.com>
2014-11-13 18:04:10 +0100

    Improved findsquare state

--- nao_smach_utils/src/nao_smach_utils/navigation_states.py
+++ nao_smach_utils/src/nao_smach_utils/navigation_states.py
@@ -6,23 +6,43 @@ from move_to_state import MoveToState
 from tts_state import SpeechState
 from home_onoff import HomeOn_SM, HomeOff_SM
 from smach import CBState
+from joint_trajectory_state import JointAngleState
 import math
 
 class FindSquare(StateMachine):
     ''' Goes around until it finds the square '''
     def __init__(self, angle=math.pi/4):
         StateMachine.__init__(self, outcomes=['succeeded', 'preempted', 'aborted'], output_keys=['square']) 
-
+        self.turns = 2
+        self.angle = angle
         with self:
-            StateMachine.add('FIND_SQUARE', ReadTopicSquare(), transitions={'succeeded': 'SPEAK_F', 'aborted': 'SPEAK_NF'})
+            StateMachine.add('FIND_SQUARE1', ReadTopicSquare(), transitions={'succeeded': 'SPEAK_F', 'aborted': 'SPEAK_NF'})
 
             text = 'I have not found the marker. I will turn around'
-            StateMachine.add('SPEAK_NF', SpeechState(text=text, blocking=False), transitions={'succeeded':'TURN_AROUND'})
+            StateMachine.add('SPEAK_NF', SpeechState(text=text, blocking=False), transitions={'succeeded':'LOOK_DOWN'})
 
             text = 'I have found the marker!'
             StateMachine.add('SPEAK_F', SpeechState(text=text, blocking=False), transitions={'succeeded':'succeeded'})
 
-            StateMachine.add('TURN_AROUND', MoveToState(objective=Pose2D(0.1, 0, angle)), transitions={'succeeded':'FIND_SQUARE'})
+            def check_turn(ud):
+                if (self.turns == 4):
+                    self.turns = 0
+                    self.angle = -self.angle
+                self.turns += 1
+                ud.objective = Pose2D(0.0, 0.0, self.angle)
+                return 'succeeded'
+            StateMachine.add('PREPARE_OBJ', CBState(check_turn, outcomes=['succeeded'], output_keys=['objective']),
+                              transitions={'succeeded':'TURN_AROUND'}, remapping={'objective': 'objective'})
+
+            StateMachine.add('TURN_AROUND', MoveToState(), transitions={'succeeded':'FIND_SQUARE1'})
+
+            StateMachine.add('LOOK_DOWN', JointAngleState(['HeadPitch'], [0.5]), transitions={'succeeded': 'FIND_SQUARE2'})
+
+            StateMachine.add('FIND_SQUARE2', ReadTopicSquare(), transitions={'succeeded': 'SPEAK_F', 'aborted': 'LOOK_UP'})
+
+            StateMachine.add('LOOK_UP', JointAngleState(['HeadPitch'], [-0.5]), transitions={'succeeded': 'FIND_SQUARE3'})
+
+            StateMachine.add('FIND_SQUARE3', ReadTopicSquare(), transitions={'succeeded': 'SPEAK_F', 'aborted': 'CHECK_TURN'})
 
 class GoToSquare(StateMachine):
      def __init__(self, dist_m_to_square=0.15, min_x_dist=0.25):
