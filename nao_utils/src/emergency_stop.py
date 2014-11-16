3094cbf368c068dc36611af507017e0373f50de2
Gerard Canal <grar.knal@gmail.com>
2014-11-16 19:41:12 +0100

    Updated emergency stop to see if making the robot think he is flying stops the walking. Need to test though

--- nao_utils/src/emergency_stop.py
+++ nao_utils/src/emergency_stop.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python
 import rospy
+import sys
 from nao_msgs.msg import TactileTouch, Bumper
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist, Vector3
 from sensor_msgs.msg import Range
 from std_srvs.srv import Empty
 
+from naoqi import ALProxy 
+from argparse import ArgumentParser
+
 ''' Stop the walking of the NAO in case the head button or foot bumpers are pressed '''
 
+IP = '127.0.0.1'
+PORT = 9559
 USE_SONAR = True
 emergency_stop_publisher = None
+memoryProxy = None
 
 
 def stop_walking(stop_method, who):
@@ -17,7 +24,17 @@ def stop_walking(stop_method, who):
         stop_method()
     else: # Publish cal
         stop_method.publish(Twist(Vector3(0.0, 0.0, 0.0), Vector3(0.0, 0.0, 0.0)))
+    # Final force stop: tell the robot it is flying (in case of blocking walks which does not stop by other means)
+    memoryProxy.raiseEvent("footContactChanged", 0.0)
+    memoryProxy.raiseEvent("footContactChanged", 1.0)
+
+    # memoryProxy.insertData("footContact", 0)
+    # memoryProxy.insertData("leftFootContact", 0)
+    # memoryProxy.insertData("rightFootContact", 0)
+    # memoryProxy.insertData("footContact", 1)
     emergency_stop_publisher.publish("Emergency stop has stopped the robot walk because of %s!" % who)
+
+
 
 def bumper_callback(data, stop_method):
     bumper = 'Left bumper' if data.bumper == data.left else 'Right bumper'
@@ -26,6 +43,8 @@ def bumper_callback(data, stop_method):
         rospy.loginfo('%s was pressed! Stop message has been sent.' % bumper)
     else:
         rospy.loginfo('%s was released. Nothing to do.' % bumper)
+
+
 
 def tactile_touch_callback(data, stop_method):
     if data.button == data.buttonFront:
@@ -41,6 +60,8 @@ def tactile_touch_callback(data, stop_method):
     else:
         rospy.loginfo('%s was released. Nothing to do.' % button)
 
+
+
 OFFSET = 0.045 # m offset from min_range
 def sonar_cb(data, arg):
     #print data.range, data.min_range+OFFSET
@@ -50,10 +71,29 @@ def sonar_cb(data, arg):
         rospy.loginfo('%s sonar detected obstacle at minimum range: %f. Stop message has been sent.' % (arg[1], data.range))
 
 
+
+
 if __name__ == '__main__':
-    global emergency_stop_publisher
+    # Parse arguments
+    parser = ArgumentParser()
+    parser.add_argument("--pip", dest="pip", default=IP,
+                      help="IP/hostname of parent broker. Default is 127.0.0.1.", metavar="IP")
+    parser.add_argument("--pport", dest="pport", default=PORT, type=int,
+                      help="port of parent broker. Default is 9559.", metavar="PORT")
+    parser.add_argument("--psonar", dest="psonar", default=USE_SONAR, type=(lambda v: v.lower() in ("yes", "true", "t", "1")),
+                      help="whether it uses the sonar for the emergency stop or not.", metavar="USE_SONAR")
+
+    args = parser.parse_args(args=rospy.myargv(argv=sys.argv)[1:])
+    IP = args.pip
+    PORT = args.pport
+    USE_SONAR = args.psonar
+    memoryProxy = ALProxy("ALMemory", IP, PORT)
+
+    # Init node with this parameters
     rospy.init_node('NAO_emergency_stop')
     rospy.loginfo('STARTING EMERGENCY STOP NODE...')
+    if not USE_SONAR:
+        rospy.logwarn('SONARS will NOT be used for the emergency stop!!')
     emergency_stop_publisher = rospy.Publisher('/emergency_stop', String, queue_size=5)
 
     try:
