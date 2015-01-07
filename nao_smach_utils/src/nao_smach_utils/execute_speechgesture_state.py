5ad4ab4832b563eb208fc221f05ab6988386fc55
Gerard Canal <grar.knal@gmail.com>
2015-01-07 12:24:36 +0100

    Improved speechgesture efficiency in non pool states

--- nao_smach_utils/src/nao_smach_utils/execute_speechgesture_state.py
+++ nao_smach_utils/src/nao_smach_utils/execute_speechgesture_state.py
@@ -2,7 +2,7 @@ __author__ = 'dani'
 from smach import Concurrence
 
 from nao_smach_utils.tts_state import SpeechFromPoolSM, SpeechState
-from nao_smach_utils.execute_choregraphe_behavior_state import ExecuteBehaviorFromPoolSM
+from nao_smach_utils.execute_choregraphe_behavior_state import ExecuteBehaviorFromPoolSM, ExecuteBehavior
 
 
 class SpeechGesture(Concurrence):
@@ -10,12 +10,14 @@ class SpeechGesture(Concurrence):
         input_keys = []
         if not textpool:
             input_keys = ['text']
+        use_bpool = True
+        if not isinstance(behavior_pool, list):
+            use_bpool = False
         Concurrence.__init__(self, outcomes=['succeeded', 'aborted', 'preempted'], input_keys=input_keys, default_outcome='aborted',
                              outcome_map={'succeeded': {'GESTURE_SPEECH': 'succeeded', 'GESTURE_MOVE': 'succeeded'}})
         with self:
             Concurrence.add('GESTURE_MOVE',
-                            #ExecuteBehavior(behavior_name),
-                            ExecuteBehaviorFromPoolSM(behavior_pool))
+                            ExecuteBehavior(behavior_pool) if not use_bpool else ExecuteBehaviorFromPoolSM(behavior_pool))
             Concurrence.add('GESTURE_SPEECH',
                             SpeechState(wait_before_speak=wait_before_speak, text=textpool, blocking=True)
                             if not textpool else SpeechFromPoolSM(wait_before_speak=wait_before_speak, pool=textpool, blocking=True))
